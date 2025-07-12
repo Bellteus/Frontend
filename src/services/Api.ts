bd406f030ad8f0f3424732ab21016aed55fcd210
Alessandro Jair Chincha Arias <89095463+AlessandroChincha@users.noreply.github.com>
2025-07-11 23:46:09 -0500

    update

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8001',
+  baseURL: 'https://backendbellteus-production-507e.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
